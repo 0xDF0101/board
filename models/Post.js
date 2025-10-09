93c339a125e85df847a876f97a72bdcf98ef72e9
eugene <hash_ff0000@naver.com>
2025-10-09 18:00:37 +0900

    Posts.js (model) 코드 리뷰

--- models/Post.js
+++ models/Post.js
@@ -1,5 +1,6 @@
 // 이건 post모델 파일임
 // 라우터 아님
+// mongoose 스키마를 통해 noSQLDB를 SQLDB처럼 만들어준다
 
 const mongoose = require('mongoose');
 
@@ -11,11 +12,13 @@ const postSchema = new mongoose.Schema(
         author: {
             // 로그인 관련
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'User', // User 모델 참조
+            ref: 'User', // User 모델 참조 --> join과 같은 개념이라고 할 수 있다!
             require: true,
         },
     },
     {
+        // 생성 시간과 마지막 수정 시간을 자동으로 관리해줌
+        // createAt과 UpdateAt 두개가 자동으로 생성되고, 사용 가능
         timestamps: true,
     }
 );
